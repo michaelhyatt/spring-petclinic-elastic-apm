279e75ab29865fe28f43b5a99b7413155c2300e7
bvader <stephen.brown@elastic.co>
2019-01-14 16:09:44 -0800

    Take out tracing..

--- run-with-apm.sh
+++ run-with-apm.sh
@@ -3,9 +3,9 @@ java -javaagent:./elastic-apm-agent-1.3.0.jar \
 -Delastic.apm.server_urls=http://localhost:8200 \
 -Delastic.apm.service_name=spring-petclinic-monolith \
 -Delastic.apm.application_packages=org.springframework.samples \
--Delastic.apm.trace_methods=org.springframework.samples.petclinic.*.*\#* \
 -jar target/spring-petclinic-2.1.0.BUILD-SNAPSHOT.jar
 
 # Add Trace Metods to above run command
 # This will trace all methods in the package. 
+#-Delastic.apm.trace_methods=org.springframework.samples.petclinic.*.*\#* \
 #
