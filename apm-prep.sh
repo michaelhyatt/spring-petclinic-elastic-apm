bb5c3ee85db0ab86ffc13ee58e1d22d1caa77222
bvader <stephen.brown@elastic.co>
2019-01-14 16:01:36 -0800

    Clean Up APM Demo

--- apm-prep.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Get the java APM agent
-curl -O  https://search.maven.org/remotecontent?filepath=co/elastic/apm/elastic-apm-agent/1.3.0/elastic-apm-agent-1.3.0.jar
-
-# Update the archive
-jar -ufv target/spring-petclinic-2.1.0.BUILD-SNAPSHOT.jar -C ./apm-demo .
--- run-normal.sh
+++ run-normal.sh
@@ -1,2 +1,1 @@
-#!/bin/bash
 java -jar target/spring-petclinic-2.1.0.BUILD-SNAPSHOT.jar
